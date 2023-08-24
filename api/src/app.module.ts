fd837bad38e5bc5d80cffb20f0daf23ae6b1ff67
Arthur <pigeon.arthur@hotmail.fr>
2023-08-24 15:27:32 +0200

    feat: added fake controller in API for front testing and connect front to back

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,12 +2,14 @@ import { Logger, Module } from '@nestjs/common';
 import { loggingMiddleware, PrismaModule } from 'nestjs-prisma';
 
 import { AuthModule } from './modules/auth';
+import { FakeModule } from './modules/fake';
 import { UserModule } from './modules/user';
 
 @Module({
   imports: [
     UserModule,
     AuthModule,
+    FakeModule,
     PrismaModule.forRoot({
       isGlobal: true,
       prismaServiceOptions: {
