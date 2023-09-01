e997633eaa6c361f1d49176620975038f52fd3bb
Bilaboz <bilaboz@protonmail.com>
2023-09-01 22:53:49 +0200

    Log incomming requests with morgan if in dev environement

--- api/src/app.middleware.ts
+++ api/src/app.middleware.ts
@@ -1,10 +1,16 @@
 import type { INestApplication } from '@nestjs/common';
 import * as compression from 'compression';
+import * as morgan from 'morgan';
 
 export function middleware(app: INestApplication): INestApplication {
+  if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('tiny'));
+  }
+
   app.use(compression());
   // app.use(passport.initialize());
   // app.use(passport.session());
   app.enableCors();
+
   return app;
 }
