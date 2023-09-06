2b267f8a76e33e0f87962ce303a50e2e09b2bc6f
rmorel <morel.romain81@gmail.com>
2023-09-06 14:20:11 +0200

    App not recognized

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 import { AuthService } from './auth.service';
+import { config } from '@/config';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Get('42')
+  @UseGuards(AuthGuard('42'))
+  auth42(@Req() req: Request, @Res() res: Response): void {
+    console.log(req);
+    console.log(res);
+    console.log(config.app42.id);
+  }
 
   @Get('42/callback')
   @UseGuards(AuthGuard('42'))
