9da6656d78d1a942c4d73763763687f24986dda8
Romain MOREL <morel.romain81@gmail.com>
2023-09-09 00:33:55 +0200

    test structure ok, need auth controller and module test. And other submodule

--- api/src/modules/auth/auth.service.spec.ts
+++ api/src/modules/auth/auth.service.spec.ts
@@ -1,14 +1,36 @@
 import { Test } from '@nestjs/testing';
 
 import { AuthService } from './auth.service';
+import { UserService } from '../user';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthService', () => {
   let authService: AuthService;
 
+  const mockJwtService = {
+
+  };
+
+  const mockUserService = {
+    getAll: jest.fn(),
+    getUnique: jest.fn(),
+    createUser: jest.fn(),
+  };
+
   beforeEach(async () => {
     // initialize a NestJS module with authService
     const module = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: UserService,
+          useValue: mockUserService,
+        },
+      ],
     }).compile();
 
     authService = module.get(AuthService);
