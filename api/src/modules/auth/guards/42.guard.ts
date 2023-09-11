1b3301b8d9ab057400f33a18be8215e7508f42c6
Bilaboz <bilaboz@protonmail.com>
2023-09-11 17:27:51 +0200

    better handling of InvalidToken exception

--- api/src/modules/auth/guards/42.guard.ts
+++ api/src/modules/auth/guards/42.guard.ts
@@ -1,5 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { TokenError } from 'passport-oauth2';
 
 @Injectable()
-export class FortyTwoAuthGuard extends AuthGuard('42') {}
+export class FortyTwoAuthGuard extends AuthGuard('42') {
+  override handleRequest<TUser = any>(err: any, user: any): TUser {
+    if (err || !user) {
+      if (err instanceof TokenError) {
+        throw new UnauthorizedException(err.message);
+      } else {
+        throw err;
+      }
+    }
+
+    return user;
+  }
+}
