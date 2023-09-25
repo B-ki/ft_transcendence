827c03b90788826f662b7c8701f9cc249e59f0d2
Bilaboz <bilaboz@protonmail.com>
2023-09-25 23:13:08 +0200

    revert something

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -2,8 +2,6 @@ import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { User } from '@prisma/client';
 
-import { config } from '@/config';
-
 import { AuthService } from './auth.service';
 import { GetUser } from './decorators';
 import { FortyTwoAuthGuard, JwtAuthGuard } from './guards';
@@ -18,19 +16,10 @@ export class AuthController {
 
   @Get('42/callback')
   @UseGuards(FortyTwoAuthGuard)
-  async auth42callback(@Req() req: any): Promise<string> {
-    const token = await this.authService.login(req.user);
-
-    const HTML = `
-    <html>
-      <script>
-        window.localStorage.setItem('token', '${token}');
-        window.location.href = 'http://' + window.location.hostname + ':${config.app.frontPort}/';
-      </script>
-    </html>
-    `;
-
-    return HTML;
+  async auth42callback(@Req() req: any): Promise<{ token: string }> {
+    return {
+      token: await this.authService.login(req.user),
+    };
   }
 
   @Get('test')
