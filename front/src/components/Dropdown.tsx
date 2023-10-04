d133bd1ba25c3e3523ce4e3eb7ae0bf27f7d8955
leobesnard <leobesnard@users.noreply.github.com>
2023-10-04 17:26:08 +0000

    Apply automatic changes

--- front/src/components/Dropdown.tsx
+++ front/src/components/Dropdown.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import { Button } from './Button';
 import { useNavigate } from 'react-router-dom';
+
+import { Button } from './Button';
 
 const navigation = [
   { name: 'Home', href: '/home' },
