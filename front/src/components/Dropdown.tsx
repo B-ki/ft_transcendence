6f866dae939310d6e196925c8d724bdbd1759ede
leobesnard <leobesnard@users.noreply.github.com>
2023-10-05 16:55:25 +0000

    Apply automatic changes

--- front/src/components/Dropdown.tsx
+++ front/src/components/Dropdown.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '@/hooks/useAuth';
+
 import DropdownImg from '@/assets/dropdown.svg';
+import { useAuth } from '@/hooks/useAuth';
 
 import { Button } from './Button';
 
