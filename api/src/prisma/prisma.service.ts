e1d8f657774c8aab7283b2804e0176e09ea178aa
Romain MOREL <morel.romain81@gmail.com>
2023-10-16 00:09:40 +0200

    testing config bug fixed

--- api/src/prisma/prisma.service.ts
+++ api/src/prisma/prisma.service.ts
@@ -3,6 +3,16 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+      ],
+    });
+  }
   async onModuleInit() {
     await this.$connect();
     Logger.log('Prisma connected', 'PrismaService');
