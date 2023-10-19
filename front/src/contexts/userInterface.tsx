02c39b07503e5f060f5deaca1e0c15a684520ac5
Romain MOREL <morel.romain81@gmail.com>
2023-10-19 22:23:26 +0200

    Stop using useApi, start using axios or fetch

---
+++ front/src/contexts/userInterface.tsx
@@ -0,0 +1,8 @@
+export interface User {
+    login: string,
+    first_name: string,
+    last_name: string,
+    email: string,
+    imageURL: string,
+    JWTtoken?: string,
+}
--- front/src/pages/OauthCallback.tsx
+++ front/src/pages/OauthCallback.tsx
@@ -1,8 +1,23 @@
+import { User } from '@/contexts/userInterface';
 import { useApi } from '@/hooks/useApi';
+import { useAuth } from '@/hooks/useAuth';
 import axios from 'axios';
 import { useEffect } from 'react';
 import { UseQueryResult } from 'react-query';
 import { useNavigate } from 'react-router-dom';
+
+export function getUser(login: string) : User {
+  /*const { data } = useApi().get('getting profile', '/user/', {
+    params: { id: login}
+  });
+  console.log(data);
+  
+ useEffect(() => {
+  fetch(`/api/user?id=${login}`)
+ }, []);*/
+  fetch(`/api/user?id=${login}`);
+  return { login: "rmorel", first_name: "a", last_name: "b", email: "c", imageURL: "d" };
+}
 
 export default function OauthCallback() {
   const navigate = useNavigate();
@@ -11,32 +26,44 @@ export default function OauthCallback() {
   const errorType = queryParameters.get('error');
   const errorMessage = queryParameters.get('error_description');
   const api = useApi();
+  const { user, login_42 } = useAuth();
 
-    if (code) {
-      interface TokenData {
-        token: string;
-        // Add other properties as needed
-      }
-
-      const { data, isLoading, isError } = api.get('send oauth code', '/auth/42/callback', {
-        params: { code: code },
-      }) as UseQueryResult<TokenData>;
-    
-      if (isLoading) {
-        return <div>Loading...</div>;
-      }
-    
-      if (isError) {
-        console.log('Error occurred');
-        navigate('/error'); // TODO : create an error page to navigate to
-      }
-    
-      if (data) {
-        console.log(data);
-        localStorage.setItem('token', data.token);
-      }
+  if (code) {
+    interface TokenData {
+      token: string;
+      login: string;
+      // Add other properties as needed
     }
-    const token = localStorage.getItem('token');
+    const { data, isLoading, isError } = api.get('send oauth code', '/auth/42/callback', {
+      params: { code: code! },
+    }) as UseQueryResult<TokenData>;
+  
+    if (isLoading) {
+      return <div>Loading...</div>;
+    }
+  
+    if (isError) {
+      console.log('Error occurred');
+      navigate('/error'); // TODO : create an error page to navigate to
+    }
+
+    if (data) {
+      console.log(data);
+      console.log("on est la");
+      localStorage.setItem('token', data.token);
+      getUser('rmorel');
+    }
+  }
+
+
+  const token = localStorage.getItem('token');
+
+/*
+  const { userProfile } = api.get('getting profile', '/user/', {
+    params: { id: data.login }
+  });
+  console.log(userProfile);
+*/
 
   return (
     <div className="left-0 top-0 flex h-screen w-screen flex-col items-center justify-center gap-40">
