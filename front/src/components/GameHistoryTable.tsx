417f39d6212c61c6891281a6a0fed03a78a3e87a
lbesnard <leobesnard8@gmail.com>
2023-11-01 17:19:22 +0100

    manger la banane

--- front/src/components/GameHistoryTable.tsx
+++ front/src/components/GameHistoryTable.tsx
@@ -1,31 +1,24 @@
-import React from 'react';
-
-const headParams = [{ title: 'You' }, { title: 'Score' }, { title: 'Opponent' }];
-
-const gameHistory = [
-  {
-    date: '2023-10-25 15:30',
-    player1Score: 3,
-    player2Score: 2,
-    duration: '5 minutes',
-  },
-  {
-    date: '2023-10-26 16:45',
-    player1Score: 2,
-    player2Score: 2,
-    duration: '6 minutes',
-  },
-  {
-    date: '2023-10-27 18:15',
-    player1Score: 5,
-    player2Score: 3,
-    duration: '8 minutes',
-  },
-];
+import React, { useEffect } from 'react';
+import { useApi } from '@/hooks/useApi';
+import { UseQueryResult } from 'react-query';
+import { userDto } from '@/dto/userDto';
+import { useState } from 'react';
+import { Button } from './Button';
 
 export const GameHistoryTable = () => {
+  const [gameHistory, setGameHistory] = useState<userDto[]>([]); // Define gameHistory as a state variable
+
+  const query = useApi().get('my games', `/game/all`) as UseQueryResult<userDto[]>;
+
+  useEffect(() => {
+    if (query.data) {
+      setGameHistory(query.data);
+      console.log(gameHistory);
+    }
+  }, []);
+
   return (
-    <table className="table-auto border-separate rounded border text-dark-1">
+    <table className="table-auto border-separate rounded border bg-dark-3 text-dark-1">
       <thead>
         <tr>
           <th className="px-4 py-2">Date</th>
@@ -37,10 +30,10 @@ export const GameHistoryTable = () => {
       <tbody>
         {gameHistory.map((game, index) => (
           <tr key={index}>
-            <td className="border px-4 py-2">{game.date}</td>
-            <td className="border px-4 py-2">{game.player1Score}</td>
-            <td className="border px-4 py-2">{game.player2Score}</td>
-            <td className="border px-4 py-2">{game.duration}</td>
+            <td className="border px-4 py-2">{}</td>
+            <td className="border px-4 py-2">{}</td>
+            <td className="border px-4 py-2">{}</td>
+            <td className="border px-4 py-2">{}</td>
           </tr>
         ))}
       </tbody>
