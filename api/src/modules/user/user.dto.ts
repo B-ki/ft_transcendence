2fe6eb8a8f80c12b930b07e976c01718229f449c
Romain MOREL <morel.romain81@gmail.com>
2023-11-03 18:51:24 +0100

    Adding IsOptionnal in DTO

--- api/src/modules/user/user.dto.ts
+++ api/src/modules/user/user.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
 
 export class UserLoginDto {
   @IsNotEmpty()
@@ -9,15 +9,19 @@ export class UserLoginDto {
 export class UpdateUserDto {
   @MaxLength(20)
   @IsString()
+  @IsOptional()
   displayName: string;
 
   @IsString()
   @MaxLength(30)
+  @IsOptional()
   description: string;
 
   @IsString()
+  @IsOptional()
   bannerUrl: string;
 
   @IsString()
+  @IsOptional()
   imageUrl: string;
 }
