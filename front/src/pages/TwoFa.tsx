4fdc96eae8d45acae66f0883d841ce68c9706ecc
Romain MOREL <morel.romain81@gmail.com>
2023-11-08 15:02:30 +0100

    Cleaning file

--- front/src/pages/TwoFa.tsx
+++ front/src/pages/TwoFa.tsx
@@ -1,19 +1,14 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { UseQueryResult } from 'react-query';
 
 import banner from '@/assets/cool-profile-picture.jpg';
 import myImage from '@/assets/d9569bbed4393e2ceb1af7ba64fdf86a.jpg';
-import { Button } from '@/components/Button';
-import { GameHistoryTable } from '@/components/GameHistoryTable';
 import { Input } from '@/components/Input';
 import { Modal } from '@/components/Modal';
 import { Navbar } from '@/components/Navbar';
 import PicUploader from '@/components/PicUploader';
-import { userDto } from '@/dto/userDto';
-import { useApi } from '@/hooks/useApi';
-import googleAuthenticatorLogo from '@/assets/GoogleAuthenticatorLogo.png'
-import { QRCodeSVG } from 'qrcode.react';
 import { QRcodeDto } from '@/dto/QRcodeDto';
+import { useApi } from '@/hooks/useApi';
 
 const inputs = [
   { id: '0', labelTxt: 'Username', inputTxt: 'Enter your username...', mandatory: true },
@@ -22,15 +17,10 @@ const inputs = [
 
 function TwoFa() {
   const [show, setShow] = useState(false);
-  const [is2FAactivated, setIs2FAactivated] = useState(false);
-  const [qrCodeUrl, setQrCodeUrl] = useState('');
-  let user: userDto | undefined = undefined;
 
-  const { data, isLoading, isError } = useApi().get(
-    'get 2 fa2',
-    '/auth/2fa/qrcode',
-    {params: {refetchOnWindowFocus: true},},
-  ) as UseQueryResult<QRcodeDto>;
+  const { data, isLoading, isError } = useApi().get('get 2 fa2', '/auth/2fa/qrcode', {
+    params: { refetchOnWindowFocus: true },
+  }) as UseQueryResult<QRcodeDto>;
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -38,8 +28,6 @@ function TwoFa() {
   if (isError) {
     return <div>Error...</div>;
   }
-  if (data) 
-  console.log(data);
   return (
     <>
       <Modal onClose={() => setShow(false)} title="Edit your profile" show={show}>
@@ -63,11 +51,10 @@ function TwoFa() {
         </div>
       </Modal>
       <Navbar />
-        
-          <div>
-            <p>2FA is activated. Scan the QR code with an authenticator app.</p>
-            <img src={data!.QrCodeUrl}/>
-          </div>
+      <div>
+        <p>2FA is activated. Scan the QR code with an authenticator app.</p>
+        <img src={data!.QrCodeUrl} alt="QRcodeFor2FA" />
+      </div>
     </>
   );
 }
