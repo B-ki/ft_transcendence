526785c7b97c3c6abfe18bbea60ad2f316f94169
lbesnard <leobesnard8@gmail.com>
2023-11-27 20:35:45 +0100

    fix

--- front/src/dto/gameDto.tsx
+++ front/src/dto/gameDto.tsx
@@ -1,6 +1,9 @@
 export interface gameDto {
-  winner: { id: number; date: string; login: string; displayName: string };
-  loser: { id: number; date: string; login: string; displayName: string };
+  id: number;
+  winnerId: number;
+  looserId: number;
+  winner: { id: number; login: string; displayName: string };
+  loser: { id: number; login: string; displayName: string };
   winnerScore: number;
   loserScore: number;
 }
